2dd6c9f22e84c902aa44b437fa4ba206461013c1
DyhaCreator <andropet228@gmail.com>
2023-11-28 21:39:13 +0500

    p

--- donut.cpp
+++ donut.cpp
@@ -3,38 +3,50 @@
 #include <cmath>
 using namespace std;
 
-int Width = 16;
-int Height = 1;
+float zoom = 7;
+int Width = 15 * zoom;
+int Height = 11 * zoom;
 int max_steps = 10;
+float min_len = 0.1;
 
-vector<float> render() {
-    vector<float>screen = vector<float>(Width * Height);
+float dSphere(float x, float y, float z, float sphereX, float sphereY, float sphereZ, float sphereR) {
+    float l = sqrt((x - sphereX) * (x - sphereX) + 
+                    (y - sphereY) * (y - sphereY) + 
+                    (z - sphereZ) * (z - sphereZ)) - sphereR;
+    return l;
+}
 
-    float sphereX = 0;
-    float sphereY = 2;
-    float sphereZ = 0;
-    float sphereR = 0.5;
+float dTorus(float x, float y, float z, float TorX, float TorY, float TorZ, float R, float r) {
+    float x1 = sqrt((x - TorX) * (x - TorX) + (y - TorY) * (y - TorY)) - R;
+    float z1 = abs(TorZ - z);
+    return sqrt(x1 * x1 + z1 * z1) - r;
+}
+
+vector<int> render(float X, float Y, float Z, float R, float r, float cRX, float cRY) {
+    vector<int>screen = vector<int>(Width * Height);
 
     float cameraX = 0;
     float cameraY = 0;
     float cameraZ = 0;
 
     for (int y = 0; y < Height; y++) {
-        float ry = 0;
+        float ry = (float)(y - Height / 2) / (float)Height + cRY;
         for (int x = 0; x < Width; x++) {
-            float rx = (float)(x - Width / 2) / (float)Width;
-            cout << rx << endl;
+            float rx = (float)(x - Width / 2) / (float)Width + cRX;
+            //cout << rx << endl;
             float rayX = cameraX;
             float rayY = cameraY;
+            float rayZ = cameraZ;
             float len = 0;
             for (int steps = 0; steps < max_steps; steps++) {
-                float l = sqrt((rayX - sphereX) * (rayX - sphereX) + (rayY - sphereY) * (rayY - sphereY)) - sphereR;
-                rayX += sin(rx) * l;
-                rayY += cos(rx) * l;
-                len += l;
+                float l = dTorus(rayX, rayY, rayZ, X, Y, Z, R, r);
+                rayX += cos(ry) * sin(rx) * l;
+                rayY += cos(ry) * cos(rx) * l;
+                rayZ += sin(ry) * l;
             }
-            screen[y * Width + x] = len;
-            cout << "---" << endl;
+            float l = dTorus(rayX, rayY, rayZ, X, Y, Z, R, r);
+            screen[y * Width + x] = (l < min_len)?1:0;
+            //cout << "---" << endl;
         }
     }
 
@@ -42,12 +54,23 @@ vector<float> render() {
 }
 
 int main() {
-    vector<float>screen = render();
-    for (int y = 0; y < Height; y++) {
-        for (int x = 0; x < Width; x++) {
-            cout << screen[y * Width + x] << " ";
+    float Ty = 1;
+    float Tz = 0;
+    float rotAnim = 0;
+    while (true) {
+        vector<int>screen = render(0, 2, Tz, 0.5, 0.02, 0, 0.2);
+        for (int y = Height * 0.3; y < Height * 0.7; y++) {
+            for (int x = 0; x < Width; x++) {\
+                if (screen[y * Width + x] == 1) {
+                    cout << '#';
+                } else {
+                    cout << " ";
+                }
+            }
+            cout << endl;
         }
-        cout << endl;
+        rotAnim += 0.01;
+        Tz = sin(rotAnim) / 5.0 + 0.4;
     }
     return 0;
 }
